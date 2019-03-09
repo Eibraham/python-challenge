2dcb325e2d5321cbaed11eedc9a4e0b2f7710ddc
user-aas <user-aas@kpmg.com.mx>
2019-03-09 12:35:06 -0600

    Challenge

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,9 @@
-##
+# Import and read file
+import os
+import csv
+
+budgetcsv = os.path.join ('..','PyBank', 'budget_data.csv')
+
+months = ['Jan','Feb','Mar','Apr','May','Jun','Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic']
+df.months.value_counts
+print ("Total Months: ", months.count())
